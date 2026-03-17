b806e50ded68ca03db69ad32bb49229306722763
Ahmed Elaraby <ahme.elaraby@asset.com.eg>
2026-03-18 01:39:31 +0200

    Refactor RabbitMQ consumers and queue configurations
    Updated RabbitMQ consumer logic to enable fair dispatch by setting `prefetchCount` to 1 globally. Simplified "weather-forecasts" queue declaration by removing `x-max-length` and `x-overflow` arguments. Introduced consumer priorities with `x-priority` for the first two consumers. Refactored to create multiple consumers dynamically, each logging its `consumerId` and simulating processing time. Adjusted `BasicConsume` calls and commented out unused configurations. Retained `BasicAck` for message acknowledgment and `Console.ReadKey()` to keep the app running.

--- NINJA.RabbitMQ.Subscriber/Program.cs
+++ NINJA.RabbitMQ.Subscriber/Program.cs
@@ -16,40 +16,47 @@ namespace NINJA.RabbitMQ.Subscriber
             };
             var connection = factory.CreateConnection();
             using var channel = connection.CreateModel();
-            channel.QueueDeclare(
-                queue: "orders",
-                durable: true,
-                exclusive: false,
-                autoDelete: true);
+            channel.BasicQos(prefetchSize: 0,prefetchCount: 1,global: false); // Set prefetch count to 1 to allow processing of 1 messages at a time
+            //channel.QueueDeclare(
+                                                                              //    queue: "orders",
+                                                                              //    durable: true,
+                                                                              //    exclusive: false,
+                                                                              //    autoDelete: true);
             channel.QueueDeclare(
                 queue: "weather-forecasts",
                 durable: false,
                 exclusive: false,
-                autoDelete: false,
-                arguments: new Dictionary<string,object>
-                {
-                    { "x-max-length", 3 }, // Set maximum queue length to 3 messages
-                    { "x-overflow", "drop-head" } // Drop the oldest message in the queue when the maximum length is reached
-                }
-                );
-            channel.BasicQos(prefetchSize: 0,prefetchCount: 3,global: false); // Set prefetch count to 2 to allow processing of 2 messages at a time
-            var consumer = new EventingBasicConsumer(channel);
-            consumer.Received += (sender,ea) =>
+                autoDelete: false);
+            for (int i = 1;i <= 3;i++)
             {
-                var body = ea.Body.ToArray();
-                var message = Encoding.UTF8.GetString(body);
-                Console.WriteLine($"Received Message: {message}");
-                channel.BasicAck(deliveryTag: ea.DeliveryTag,multiple: false);
-                //channel.BasicNack(deliveryTag: ea.DeliveryTag,multiple: false,requeue: true); = channel.BasicReject(deliveryTag: ea.DeliveryTag,requeue: true);
-            };
-            channel.BasicConsume(
-                queue: "orders",
-                autoAck: false,
-                consumer: consumer);
-            channel.BasicConsume(
-               queue: "weather-forecasts",
-               autoAck: false,
-               consumer: consumer);
+                // fair dispatch
+                var consumer = new EventingBasicConsumer(channel);
+                var consumerId = i;
+                consumer.Received += (sender,ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine($"Consumer {consumerId} Received Message: {message}");
+                    Thread.Sleep(1000); // Simulate processing time
+                    channel.BasicAck(deliveryTag: ea.DeliveryTag,multiple: false);
+                    //channel.BasicNack(deliveryTag: ea.DeliveryTag,multiple: false,requeue: true); = channel.BasicReject(deliveryTag: ea.DeliveryTag,requeue: true);
+                };
+                var argys = new Dictionary<string,object>();
+                if(i == 1)
+                    argys.Add("x-priority",10);
+                if(i == 2)
+                    argys.Add("x-priority",5);
+                //channel.BasicConsume(
+                //    queue: "orders",
+                //    autoAck: false,
+                //    consumer: consumer);
+                channel.BasicConsume(
+                   queue: "weather-forecasts",
+                   autoAck: false,
+                   consumerTag:$"Message{i}",
+                   arguments:argys,
+                   consumer: consumer);
+            }
             Console.ReadKey();
         }
     }
