6517105ea3f6aafe37e1c897a01ef81ab9cd9c7a
Ahmed Elaraby <ahme.elaraby@asset.com.eg>
2026-03-21 21:59:23 +0200

    Simplify RabbitMQ consumer logic and update credentials
    Updated RabbitMQ credentials in `appsettings.json`:
    - Changed `UserName` to `"ninja"` and `Password` to `"P@ssw0rd"`.
    
    Refactored `Program.cs`:
    - Simplified consumer logic to use a single consumer.
    - Updated `channel.QueueDeclare` to use `"weather-forecasts"` queue.
    - Removed multi-consumer handling, prioritization, and unused code.
    - Commented out `Thread.Sleep` and `channel.BasicQos` calls.
    - Simplified `consumer.Received` event handler logging.
    
    These changes streamline the application and reduce complexity.

--- NINJA.RabbitMQ.Subscriber/Program.cs
+++ NINJA.RabbitMQ.Subscriber/Program.cs
@@ -16,12 +16,12 @@ namespace NINJA.RabbitMQ.Subscriber
             };
             var connection = factory.CreateConnection();
             using var channel = connection.CreateModel();
-            channel.BasicQos(prefetchSize: 0,prefetchCount: 1,global: false); // Set prefetch count to 1 to allow processing of 1 messages at a time
+            //channel.BasicQos(prefetchSize: 0,prefetchCount: 1,global: false); // Set prefetch count to 1 to allow processing of 1 messages at a time
             //channel.QueueDeclare(
-                                                                              //    queue: "orders",
-                                                                              //    durable: true,
-                                                                              //    exclusive: false,
-                                                                              //    autoDelete: true);
+            //    queue: "orders",
+            //    durable: true,
+            //    exclusive: false,
+            //    autoDelete: true);
             channel.QueueDeclare(
                 queue: "weather-forecasts",
                 durable: false,
@@ -31,36 +31,36 @@ namespace NINJA.RabbitMQ.Subscriber
             //    queue: "weather-forecasts",
             //    exchange: "weather-exchange",
             //    routingKey: "forecast.*");
-            for (int i = 1;i <= 3;i++)
+            //for (int i = 1;i <= 3;i++)
+            //{
+            // fair dispatch
+            var consumer = new EventingBasicConsumer(channel);
+            //var consumerId = i;
+            consumer.Received += (sender,ea) =>
             {
-                // fair dispatch
-                var consumer = new EventingBasicConsumer(channel);
-                var consumerId = i;
-                consumer.Received += (sender,ea) =>
-                {
-                    var body = ea.Body.ToArray();
-                    var message = Encoding.UTF8.GetString(body);
-                    Console.WriteLine($"Consumer {consumerId} Received Message: {message}");
-                    Thread.Sleep(1000); // Simulate processing time
-                    channel.BasicAck(deliveryTag: ea.DeliveryTag,multiple: false);
-                    //channel.BasicNack(deliveryTag: ea.DeliveryTag,multiple: false,requeue: true); = channel.BasicReject(deliveryTag: ea.DeliveryTag,requeue: true);
-                };
-                var argys = new Dictionary<string,object>();
-                if(i == 1)
-                    argys.Add("x-priority",10);
-                if(i == 2)
-                    argys.Add("x-priority",5);
-                //channel.BasicConsume(
-                //    queue: "orders",
-                //    autoAck: false,
-                //    consumer: consumer);
-                channel.BasicConsume(
-                   queue: "weather-forecasts",
-                   autoAck: false,
-                   consumerTag:$"Message{i}",
-                   arguments:argys,
-                   consumer: consumer);
-            }
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                //Console.WriteLine($"Consumer {consumerId} Received Message: {message}");
+                Console.WriteLine($"Consumer Received Message: {message}");
+                //Thread.Sleep(1000); // Simulate processing time
+                channel.BasicAck(deliveryTag: ea.DeliveryTag,multiple: false);
+                //channel.BasicNack(deliveryTag: ea.DeliveryTag,multiple: false,requeue: true); = channel.BasicReject(deliveryTag: ea.DeliveryTag,requeue: true);
+            };
+            //var argys = new Dictionary<string,object>();
+            //if(i == 1)
+            //    argys.Add("x-priority",10);
+            //if(i == 2)
+            //    argys.Add("x-priority",5);
+            //channel.BasicConsume(
+            //    queue: "orders",
+            //    autoAck: false,
+            //    consumer: consumer);
+            channel.BasicConsume(
+               queue: "weather-forecasts",
+               autoAck: false,
+               //arguments:argys,
+               consumer: consumer);
+            // }
             Console.ReadKey();
         }
     }
